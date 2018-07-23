179655ad236d01bbc77b9d7ceb28c073d6c219c4
Khanh Mai <kaden.mai@live.com>
2018-07-22 23:53:28 -0700

    Add sample for all metrics

--- js/popup.js
+++ js/popup.js
@@ -3,8 +3,10 @@ $(function () {
     
     $(".popup img").click(function () {
         var $src = $(this).attr("src");
+        var $title = $(this).attr("title");
         $(".show").fadeIn();
         $(".img-show img").attr("src", $src);
+        $(".img-show div")[0].innerHTML = '<h2 class="metricTitle">' + $title + '</h2>'
     });
     
     $("span, .overlay").click(function () {
